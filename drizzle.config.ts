ddd4d6a56f0832e8a6aadc05af939bb76eddb02b
David Blanco <david.blanco.f@astara.com>
2023-09-09 22:07:38 +0200

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "drizzle-kit";
+import { loadEnv } from "vite";
+const { POSTGRES_URL } = loadEnv(process.env.POSTGRES_URL as string, process.cwd(), "");
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+
+  dbCredentials: {
+    connectionString: POSTGRES_URL,
+  },
+} satisfies Config;
