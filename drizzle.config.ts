9f49e5314c16e2d87e8dd9480d44b424f57efe56
David Blanco <david.blanco.f@astara.com>
2023-09-10 15:10:51 +0200

    refactor

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,21 @@
 import type { Config } from "drizzle-kit";
 import { loadEnv } from "vite";
-const { POSTGRES_URL } = loadEnv(process.env.POSTGRES_URL as string, process.cwd(), "");
+const { env } = process;
+
+const { POSTGRES_PASSWORD } = loadEnv(env.POSTGRES_PASSWORD as string, process.cwd(), "");
+const { POSTGRES_HOST } = loadEnv(env.POSTGRES_HOST as string, process.cwd(), "");
+const { POSTGRES_DATABASE } = loadEnv(env.POSTGRES_DATABASE as string, process.cwd(), "");
+const { POSTGRES_USER } = loadEnv(env.POSTGRES_USER as string, process.cwd(), "");
 
 export default {
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: `${POSTGRES_URL}?ssl=true`
+    host: POSTGRES_HOST,
+    database: POSTGRES_DATABASE,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    ssl: true,
   },
 } satisfies Config;
